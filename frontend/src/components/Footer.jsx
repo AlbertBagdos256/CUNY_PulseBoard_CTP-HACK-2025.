aea48d78a954fb36083970bcc793b481b8b8bcc2
Wayel Hidais <wayelhidais@safas-air.mynetworksettings.com>
2025-08-20 19:10:10 -0400

    Fix logo import for header and footer components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.png";
+import logo from "../assets/logov1.png";
 
 const Footer = () => {
   return (
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
-import logo from "../assets/logo.png";
+import logo from "../assets/logov1.png";
 
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
